6107c81ea68097dea4d1c0c909ae8b0ebfd0eb89
Hish5 <112846682+Hish5@users.noreply.github.com>
2024-03-20 08:44:26 +0530

    Delete day 3 directory

--- day 3/practical session day 3/practical session day 3/1) c) Arithmetic exception tan 90.java
+++
@@ -1,7 +0,0 @@
-public class Java {
-    public static void main(String[] args) {
-        double angleInDegrees = 90;
-        double tangent = Math.tan(Math.toRadians(angleInDegrees));
-        System.out.println("Tangent of 90 degrees: " + tangent);
-    }
-}
